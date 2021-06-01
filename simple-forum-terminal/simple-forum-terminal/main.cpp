a5fb7244199286726c3326e9abffd9417d0ca413
quanphanhong <18520239@gm.uit.edu.vn>
2021-06-01 21:24:16 +0700

    Update .gitignore and initialize the project

---
+++ simple-forum-terminal/simple-forum-terminal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple-forum-terminal
+//
+//  Created by Quan Phan on 01/06/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
